25e8f210f14f3cbb80930cc05b636f49bf41ab34
daniel <daniol1618@gmail.com>
2026-01-28 09:36:11 -0500

    feat: *ngIf Directive with else template used instead of @if @else for old version compatibility

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,17 +1,17 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatCardAvatar, MatCardModule } from '@angular/material/card';
 import { COURSES } from '../../../public/assets/data';
-import { CommonModule, NgFor } from '@angular/common';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-courses',
-  imports: [MatCardModule, NgFor],
+  imports: [MatCardModule, NgFor, NgIf],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
 })
 export class CoursesComponent {
   courses = [...COURSES.courses];
-
+  
   @Input()
   title: string | undefined;
 
@@ -20,6 +20,10 @@ export class CoursesComponent {
 
   onSeeMore() {
     this.element.emit();
+  }
+
+  areThereCourses(): boolean {
+    return this.courses && this.courses.length > 0;
   }
 
 }
